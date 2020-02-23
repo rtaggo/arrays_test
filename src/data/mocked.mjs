a38bf2ac6772139a327e9de009f3dee0855de7d3
rteina <rteina@galigeo.com>
2020-02-23 17:35:55 +0100

    add random data

--- src/data/mocked.mjs
+++ src/data/mocked.mjs
@@ -11,3 +11,28 @@ export const data2 = [
   { a: 'picsou', z: 0 },
   { a: 'donald', z: 2 },
 ];
+
+export const randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+export const generateData = (nb, extraFields) => {
+  let arr = [];
+  extraFields = extraFields || [];
+  let setUnique = new Set();
+  for (let i = 0; i < nb; i++) {
+    const aname = `name_${randomNumber(0, nb)}`;
+    if (!setUnique.has(aname)) {
+      let elt = {
+        name: aname,
+        a: `a_${i}`,
+      };
+      extraFields.forEach(f => {
+        elt[f] = `f_${randomNumber(0, 100)}`;
+      });
+      arr.push(elt);
+      setUnique.add(aname);
+    }
+  }
+  return arr;
+};
--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,21 @@
 import { printArray } from './utils/print.mjs';
-import { data1, data2 } from './data/mocked.mjs';
+//import { data1, data2, generateData } from './data/mocked.mjs';
+import { randomNumber, generateData } from './data/mocked.mjs';
 
 console.log('Arrays playground');
 
+let minData = 5;
+let maxData = 20;
+let nbData1 = randomNumber(minData, maxData);
+let nbData2 = randomNumber(minData, maxData);
+let data1 = generateData(nbData1, ['loulou']);
+let data2 = generateData(nbData2, ['riri', 'fifi']);
+data1.sort((a, b) => {
+  return a.name.localeCompare(b.name);
+});
+data2.sort((a, b) => {
+  return a.name.localeCompare(b.name);
+});
 //console.log(`data1: ${JSON.stringify(data1)}`);
 //console.log(`data2: ${JSON.stringify(data2)}`);
 printArray(data1, 'Array 1');
@@ -10,15 +23,19 @@ printArray(data2, 'Array 2');
 
 let counter = 0;
 let merged = [];
-let diff1 = data1.filter(d1 => {
-  return data2.some(d2 => {
-    const matched = d2['a'] === d1['a'];
+let diff1 = data2.filter(d1 => {
+  return data1.some(d2 => {
+    const matched = d2['name'] === d1['name'];
     if (matched) {
       merged.push({ ...d1, ...d2 });
     }
     counter++;
     return matched;
   });
+});
+
+merged.sort((a, b) => {
+  return a.name.localeCompare(b.name);
 });
 
 printArray(diff1, 'Diff');
@@ -30,4 +47,5 @@ Size diff1 : ${diff1.length}
 Size merged : ${merged.length}
 Nb Loops: ${counter}
 `;
+
 console.log(resume);
